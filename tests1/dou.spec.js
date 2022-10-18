f3daa329e8e5cbe32f6b52d1306b8bf5fd176e8e
Yuri Andorschuk <yuan@opinov8.com>
2022-10-18 10:47:19 +0300

    first commit

---
+++ tests1/dou.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+test('homepage has DOU in title and get started link linking to the intro page', async ({ page }) => {
+    await page.goto('https://dou.ua//');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Спільнота програмістів/);
+  
+    // create a locator
+    const Logo = page.locator('.logo >> a');
+  
+    // Expect an attribute "to be strictly equal" to the value.
+    await expect(Logo).toHaveAttribute('href', 'https://dou.ua/');
+  
+    // An image should be visible in slider
+    await expect(page.locator('.slide >> img').toBeVisible)
+    // Click on login, login form should appear
+   await page.locator('#login-link').click()
+   await expect(page.locator('#_loginDialog')).toBeVisible()
+  });
